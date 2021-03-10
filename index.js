cf7e1eaaf37d07b3231799c660a1ca8b816903fa
Minibumphead <minibumphead281@gmail.com>
2021-03-10 14:22:48 +0100

    added .env

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ import cors from 'cors'
 import UserRouter from './routes/usersRouter.js'
 import todosRouter from './routes/todosRouter.js'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+// using this allows devs to access env variables stored in .env file. Access with process.senv.<varname>
+dotenv.config()
+
+
 // initialize app
 const app = express()
 
 //configure Port
 const PORT = 5000
-mongoose.connect('mongodb+srv://Minibumphead:catdogmouse!!!999@cluster0.5gnyy.mongodb.net/counter_db?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false}).then(
+mongoose.connect(process.env.DB_SECRET, { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false}).then(
     () => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))    
 ).catch(
     () => console.log("asdfaldfadsf")
